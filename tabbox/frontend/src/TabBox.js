acd8dee09da5bb294f1300db0a47b5c9024752e8
seojeongHHHui <sjhui0523@gmail.com>
2024-07-12 09:54:37 +0900

    tabbox #finish

--- tabbox/frontend/src/TabBox.js
+++ tabbox/frontend/src/TabBox.js
@@ -2,10 +2,24 @@ import React from 'react';
 import Tabs from './Tabs';
 import TabView from './TabView';
 
-function TabBox({tabs}) {
+function TabBox() {
+
+    const tabs = [
+        {no: 1, name: '메뉴1', active: false, contents: '메뉴1의 뷰 내용'},
+        {no: 2, name: '메뉴2', active: false, contents: '메뉴2의 뷰 내용'},
+        {no: 3, name: '메뉴3', active: true, contents: '메뉴3의 뷰 내용'},
+        {no: 4, name: '메뉴4', active: false, contents: '메뉴4의 뷰 내용'},
+        {no: 5, name: '메뉴5', active: false, contents: '메뉴5의 뷰 내용'},
+        {no: 6, name: '메뉴6', active: false, contents: '메뉴6의 뷰 내용'},
+        {no: 7, name: '메뉴7', active: false, contents: '메뉴7의 뷰 내용'}
+    ];
+
     return (
         <div className={'tab-box'}>
-            <Tabs tabs={tabs}/>
+            <Tabs tabs={tabs.map(e => {
+                const {contents, ...rest} = e;
+                return rest;
+            })}/>
             <TabView />
         </div>
     );
