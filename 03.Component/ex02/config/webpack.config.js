20f538e91d36982ce4ee5f0d71d958e0b41a4163
seojeongHHHui <sjhui0523@gmail.com>
2024-07-12 16:19:24 +0900

    styling #finish

--- 03.Component/ex02/config/webpack.config.js
+++ 03.Component/ex02/config/webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = function(env) {
     return {
         mode: "none",
-        entry: path.resolve(`src/index.js`),
+        entry: path.resolve(`src/${env.src}/index.js`),
         output: {
             path: path.resolve('public'),
             filename: 'assets/js/main.js',
@@ -24,7 +24,7 @@ module.exports = function(env) {
                     {
                         loader: 'css-loader',
                         options: {
-                            modules: true
+                            modules: env['css-module'] !== 'false'
                         }
                     }, 
                     'sass-loader'
