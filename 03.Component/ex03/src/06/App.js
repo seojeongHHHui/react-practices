2c8fd115145cf9e2caa8d50ef157991d85571f49
seojeongHHHui <sjhui0523@gmail.com>
2024-07-15 17:06:39 +0900

    event #finish

--- 03.Component/ex03/src/06/App.js
+++ 03.Component/ex03/src/06/App.js
@@ -8,9 +8,17 @@ export default class App extends Component {
 
     render() {
         return (
-            <div className={'App'}>
-                <div>
+            <div
+                ref={ref => this.outerRef = ref}
+                className={'App'}
+                onScroll={() => {
+                    console.log(this.outerRef.scrollTop, this.outerRef.clientHeight, this.innerRef.clientHeight)
+                }}>
+                <div ref={ref => this.innerRef = ref}>
                     <ul>
+                    {
+                        Array.from({length:100}, (_, i) => i+1).map(e => <li key={e}>{`아이텀 ${e}입니다.`}</li>)
+                    }
                     </ul>
                 </div>
             </div>
